d3cbeb828721a4b1885488efaf914cda8618cb16
natiqhaciyef <natiq00h2272@gmail.com>
2023-05-07 01:59:02 +0400

    Initial Commit

---
+++ SwiftLearning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftLearning
+//
+//  Created by Natiq Haciyev on 07.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
